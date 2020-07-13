1b4334223b3b419cdecc3b762e2703bd9aa770c1
Tom Scharstein <trscharstein@gmail.com>
2020-07-12 21:46:05 -0700

    Adding vuepress documentation foundation

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,57 @@
+const { description } = require("../../package");
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: "Lottie API",
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+    ],
+  ],
+
+  /** * Automatically inject h2 (##) headers as sidebar links: */
+  markdown: {
+    extractHeaders: ["h2"],
+  },
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: "",
+    editLinks: false,
+    docsDir: "",
+    editLinkText: "",
+    lastUpdated: false,
+    logo: "",
+    nav: [
+      {
+        text: "Home",
+        link: "/",
+      },
+    ],
+    sidebar: [["/", "Home"], "/api/"],
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [],
+};
